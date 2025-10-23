4cb7874907ad09ff4260d281d3b4c8468d761f4e
Sameer-Sarrainodu <sameer339977.com>
2025-10-23 15:07:42 +0530

    terraformeks

--- 80-eks/main.tf
+++ 80-eks/main.tf
@@ -48,29 +48,29 @@ module "eks" {
         AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
     }
     }
-    # green = {
-    #   # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-    #   ami_type       = "AL2023_x86_64_STANDARD" # user name is ec2-user
-    #   instance_types = ["m5.xlarge"]
+    green = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD" # user name is ec2-user
+      instance_types = ["m5.xlarge"]
 
-    #   min_size     = 2
-    #   max_size     = 10
-    #   desired_size = 2
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
 
-    #   iam_role_additional_policies = {
-    #     AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-    #     AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-    #     AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
-    #   }
+      iam_role_additional_policies = {
+        AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+        AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+      }
 
-      /* taints = {
+      taints = {
         upgrade = {
           key = "upgrade"
           value  = "true"
           effect = "NO_SCHEDULE"
         }
-      } */
-    # }
+      }
+    }
 
   }
 
